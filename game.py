419a1e0d1c0b4af813a6203e5d05c453d13c04ed
seongsu <kss6609@naver.com>
2020-12-30 23:38:47 +0900

    (ADD) Theme List

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import sys
 import random
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon, QPainter, QPen, QColor, QBrush
+from PyQt5.QtGui import QIcon, QPainter, QPen, QColor, QBrush, QPalette
 from PyQt5.QtCore import Qt, QTimer
 
 SPEED = 50  # 50:easy, 40:normal, 30:hard
@@ -16,6 +16,20 @@ BOARD_HCNT = BOARD_H / UNIT_CELL_LEN
 dir_keys = [Qt.Key_Up, Qt.Key_Right, Qt.Key_Down, Qt.Key_Left]
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
+
+themes = [
+    {'head':QColor(Qt.black), 'body':QColor(Qt.darkGreen),
+     'apple':QColor(Qt.red), 'background':QColor('#e7e7e7'),
+     'score':'black'},
+
+    {'head':QColor(Qt.darkGreen), 'body':QColor('#2dd014'),
+    'apple':QColor('#ff4d41'), 'background':QColor('#a6e8ff'),
+     'score':'blue'},
+
+    {'head':QColor(Qt.yellow), 'body':QColor(Qt.darkGreen),
+    'apple':QColor(Qt.red), 'background':QColor('#34009e'),
+     'score':'white'}
+]
 
 class Snake:
     def __init__(self):
@@ -36,16 +50,19 @@ class Snake:
 class GameBoard(QWidget):
     def __init__(self):
         super().__init__()
-        self.color_head = QColor(Qt.black)
-        self.color_body = QColor("#088c06")
-        self.color_apple = QColor(Qt.red)
-        self.color_background = QColor("#e7e7e7")
         self.resize(BOARD_W, BOARD_H)
         self.initGame()
         self.newGame()
 
     def initGame(self):
+        self.theme = 2
+        pal = QPalette()
+        pal.setColor(QPalette.Window, themes[self.theme]['background'])
+        self.setAutoFillBackground(True)
+        self.setPalette(pal)
+
         self.lbl_score = QLabel(self)
+        self.lbl_score.setStyleSheet('color: ' + themes[self.theme]['score'])
         self.lbl_score.move(BOARD_W - 100, 0)
         self.lbl_score.resize(100, 30)
         font = self.lbl_score.font()
@@ -109,24 +126,91 @@ class GameBoard(QWidget):
         qp.end()
 
     def drawGame(self, qp):
-        # draw boundary
-        qp.fillRect(0, 0, BOARD_W, BOARD_H, self.color_background)
-
         # draw apple
         qp.setPen(Qt.NoPen)
-        qp.setBrush(self.color_apple)
+        qp.setBrush(themes[self.theme]['apple'])
         qp.drawRect(self.apple[0] * UNIT_CELL_LEN, self.apple[1] * UNIT_CELL_LEN, UNIT_CELL_LEN, UNIT_CELL_LEN)
 
         # draw snake body
         body = self.snake.body[1:]
-        qp.setBrush(self.color_body)
+        qp.setBrush(themes[self.theme]['body'])
         for cell in body:
             qp.drawRect(cell[0] * UNIT_CELL_LEN, cell[1] * UNIT_CELL_LEN, UNIT_CELL_LEN, UNIT_CELL_LEN)
 
         # draw snake head
         head = self.snake.body[0]
-        qp.setBrush(self.color_head)
+        qp.setBrush(themes[self.theme]['head'])
         qp.drawRect(head[0] * UNIT_CELL_LEN, head[1] * UNIT_CELL_LEN, UNIT_CELL_LEN, UNIT_CELL_LEN)
+
+class GameWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Snake Game')
+        self.setFixedSize(BOARD_W, BOARD_H + UNIT_CELL_LEN)     #self.resize(BOARD_W, BOARD_H)
+        self.center()
+        self.initMenu()
+        self.gb = GameBoard()
+        self.setCentralWidget(self.gb)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def initMenu(self):
+        action_theme = QAction(QIcon('image/theme.PNG'), 'Theme', self)
+        action_theme.setShortcut('Ctrl+R')
+        action_theme.triggered.connect(self.slot_theme)
+
+        action_setting = QAction(QIcon('image/setting.PNG'), 'Setting', self)
+        action_setting.setShortcut('Ctrl+O')
+        action_setting.triggered.connect(self.slot_setting)
+
+        action_heart = QAction(QIcon('image/heart.PNG'), '???', self)
+        action_heart.setShortcut('Ctrl+H')
+        action_heart.triggered.connect(self.slot_heart)
+
+        action_quit = QAction(QIcon('image/exit.png'), 'Quit', self)
+        action_quit.setShortcut('Ctrl+Q')
+        action_quit.triggered.connect(self.close)
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('File')
+        filemenu.addAction(action_theme)
+        filemenu.addAction(action_setting)
+        #filemenu.addAction(action_heart)
+        filemenu.addAction(action_quit)
+
+    def keyPressEvent(self, e):
+        if e.key() in dir_keys:
+            if self.gb.gameState == 'stop':
+                sd = dir_keys.index(e.key())
+                if (sd + 2) % 4 != self.gb.snake.dir:
+                    self.gb.snake.ndir = sd
+                    self.gb.startGame()
+
+            elif self.gb.gameState == 'playing':
+                nd = dir_keys.index(e.key())
+                if (nd % 2 == 0) ^ (self.gb.snake.dir % 2 == 0):
+                    self.gb.snake.ndir = nd
+
+    def slot_theme(self):
+        print("theme")
+        QMessageBox().about(self, 'theme', 'to be implemented')
+
+    def slot_setting(self):
+        print('setting!')
+        opt = OptionWindow()
+        opt.exec_()
+
+    def slot_heart(self):
+        print("heart")
+        QMessageBox().about(self, '???', '안녕 ㅎㅎㅎ')
 
 class OptionWindow(QDialog):
     def __init__(self):
@@ -184,68 +268,6 @@ class OptionWindow(QDialog):
             SPEED = SPD_LIST[2]
         print('new speed : %d' % SPEED)
         self.close()
-
-class GameWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('Snake Game')
-        self.setFixedSize(BOARD_W, BOARD_H + UNIT_CELL_LEN)     #self.resize(BOARD_W, BOARD_H)
-        self.center()
-        self.initMenu()
-        self.gb = GameBoard()
-        self.setCentralWidget(self.gb)
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-    def initMenu(self):
-        action_rank = QAction(QIcon('image/crown.PNG'), 'Rank', self)
-        action_rank.setShortcut('Ctrl+R')
-        action_rank.triggered.connect(self.slot_rank)
-
-        action_setting = QAction(QIcon('image/setting.PNG'), 'Setting', self)
-        action_setting.setShortcut('Ctrl+O')
-        action_setting.triggered.connect(self.slot_setting)
-
-        action_quit = QAction(QIcon('image/exit.png'), 'Quit', self)
-        action_quit.setShortcut('Ctrl+Q')
-        action_quit.triggered.connect(self.close)
-
-        menubar = self.menuBar()
-        menubar.setNativeMenuBar(False)
-        filemenu = menubar.addMenu('File')
-        filemenu.addAction(action_rank)
-        filemenu.addAction(action_setting)
-        filemenu.addAction(action_quit)
-
-    def keyPressEvent(self, e):
-        if e.key() in dir_keys:
-            if self.gb.gameState == 'stop':
-                sd = dir_keys.index(e.key())
-                if (sd + 2) % 4 != self.gb.snake.dir:
-                    self.gb.snake.ndir = sd
-                    self.gb.startGame()
-
-            elif self.gb.gameState == 'playing':
-                nd = dir_keys.index(e.key())
-                if (nd % 2 == 0) ^ (self.gb.snake.dir % 2 == 0):
-                    self.gb.snake.ndir = nd
-
-    def slot_rank(self):
-        print("rank!")
-        QMessageBox().about(self, 'rank', 'to be implemented')
-
-    def slot_setting(self):
-        print('setting!')
-        opt = OptionWindow()
-        opt.exec_()
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- test.py
+++ test.py
@@ -1,73 +1,37 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFrame, QColorDialog
+from PyQt5.QtGui import QColor
 
-class LogInDialog(QDialog):
+
+class MyApp(QWidget):
+
     def __init__(self):
         super().__init__()
-        self.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
-        self.setupUI()
+        self.initUI()
 
-        self.id = None
-        self.password = None
+    def initUI(self):
+        col = QColor(0, 0, 0)
 
-    def setupUI(self):
-        self.setGeometry(1100, 200, 300, 100)
-        self.setWindowTitle("Sign In")
-        self.setWindowIcon(QIcon('icon.png'))
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(30, 30)
+        self.btn.clicked.connect(self.showDialog)
 
-        label1 = QLabel("ID: ")
-        label2 = QLabel("Password: ")
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet('QWidget { background-color: %s }' % col.name())
+        self.frm.setGeometry(130, 35, 100, 100)
 
-        self.lineEdit1 = QLineEdit()
-        self.lineEdit2 = QLineEdit()
-        self.pushButton1= QPushButton("Sign In")
-        self.pushButton1.clicked.connect(self.pushButtonClicked)
+        self.setWindowTitle('Color Dialog')
+        self.setGeometry(300, 300, 250, 180)
+        self.show()
 
-        layout = QGridLayout()
-        layout.addWidget(label1, 0, 0)
-        layout.addWidget(self.lineEdit1, 0, 1)
-        layout.addWidget(self.pushButton1, 0, 2)
-        layout.addWidget(label2, 1, 0)
-        layout.addWidget(self.lineEdit2, 1, 1)
+    def showDialog(self):
+        col = QColorDialog.getColor()
 
-        self.setLayout(layout)
+        if col.isValid():
+            self.frm.setStyleSheet('QWidget { background-color: %s }' % col.name())
 
-    def pushButtonClicked(self):
-        self.id = self.lineEdit1.text()
-        self.password = self.lineEdit2.text()
-        self.close()
 
-class MyWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setupUI()
-
-    def setupUI(self):
-        self.setGeometry(800, 200, 300, 300)
-        self.setWindowTitle("PyStock v0.1")
-        self.setWindowIcon(QIcon('icon.png'))
-
-        self.pushButton = QPushButton("Sign In")
-        self.pushButton.clicked.connect(self.pushButtonClicked)
-        self.label = QLabel()
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.pushButton)
-        layout.addWidget(self.label)
-
-        self.setLayout(layout)
-
-    def pushButtonClicked(self):
-        dlg = LogInDialog()
-        dlg.exec_()
-        id = dlg.id
-        password = dlg.password
-        self.label.setText("id: %s password: %s" % (id, password))
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
-    app.exec_()
+    ex = MyApp()
+    sys.exit(app.exec_())
